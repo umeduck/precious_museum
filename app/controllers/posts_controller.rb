c618a65559ca96f74cb3250088bb80c171f15f89
umeduck <ume_duck@outlook.jp>
2022-12-02 07:14:19 +0900

    投稿詳細ページの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
+  before_action :post_find, only: [:show, :edit, :update, :destroy]
+  before_action :only_user, only: [:edit, :destroy]
+
   def index
+    @posts = Post.all
   end
 
   def new
@@ -15,9 +19,36 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(params[:id])
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
   private
 
   def post_params
     params.require(:post).permit(:title, :text, :image).merge(user_id: current_user.id)
   end
+
+  def post_find
+    @post = Post.find(params[:id])
+  end
+
+  def only_user
+    redirect_to posts_path unless current_user.id == @post.user_id
+  end
+
 end
